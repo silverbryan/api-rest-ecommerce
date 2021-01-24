58ab29df86fb67c7d75fb70780070ab2a219a0da
silverbryan <soporte.g4a@gmail.com>
2021-01-24 16:24:00 -0300

    arreglando error de puertos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 conn.sync({ force: false }).then(() => {
-    server.listen(3001, () => {
-        console.log("%s listening at 3001"); // eslint-disable-line no-console
+    server.listen(process.env.PORT || 3001, () => {
+        console.log(`listening at ${process.env.PORT}"); // eslint-disable-line no-console
     });
 });
